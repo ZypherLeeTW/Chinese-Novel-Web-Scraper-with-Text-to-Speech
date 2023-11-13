fae79154c0768dc339c26e0a2502079f047a22c9
李宇承_EasonLee <119650549+ZypherLeeTW@users.noreply.github.com>
2023-11-14 03:41:06 +0800

    Update uukanshu_novel_web_crawler.py

--- uukanshu_novel_web_crawler.py
+++ uukanshu_novel_web_crawler.py
@@ -1,60 +1,75 @@
 import random
 from datetime import datetime
 import requests
-from fake_useragent import UserAgent
-from bs4 import BeautifulSoup
-from opencc import OpenCC
+from fake_useragent import UserAgent  # Generate random user agent
+from bs4 import BeautifulSoup  # Parse HTML
+from opencc import OpenCC  # Simplified Chinese to Traditional Chinese conversion
 import asyncio
-import edge_tts
+import edge_tts  # Text-to-speech
 import os
 import re
 
-# pip install requests
-# pip install fake-useragent
-# pip install beautifulsoup4
-# pip install opencc-python-reimplemented
-
+# User agent object
 ua = UserAgent()
+
+# Current time
 now = datetime.now()
+
+# Random seed
 seed = now.timestamp()
 random.seed(seed)
-converter = OpenCC('s2t')  # 簡體轉繁體
 
-headers = headers = {
+# Simplified to Traditional Chinese conversion object
+converter = OpenCC('s2t')
+
+# HTTP request headers
+headers = {
     "Accept-Encoding": "gzip, deflate, br",
     "Accept-Language": "zh-TW,zh;q=0.9",
     "Sec-Fetch-Dest": "document",
     "Sec-Fetch-Mode": "navigate",
     "Sec-Fetch-Site": "none",
     "Upgrade-Insecure-Requests": "1",
-    "User-Agent": ua.random,  # 使用者代理
+    "User-Agent": ua.random,
     "Connection": "keep-alive",
     "Host": "www.uukanshu.com",
     "Referer": "https://www.uukanshu.com/",
 }
+
+# Get page source code
 
 
 def get_page(url):
     response = requests.get(url, headers=headers)
     return response.text
 
+# Parse page data
+
 
 def page_data(soup):
     page = get_page(url)
     soup = BeautifulSoup(page, 'html.parser')
+
     timu = converter.convert(soup.find(id="timu").text)
     content = converter.convert(soup.find('div', id='contentbox').text)
+
     next_link = 'https://www.uukanshu.com' + soup.find('a', id='next')['href']
-    # 去除符號
-    content = re.sub(r'[a-z]', '', content)
+
+    # Remove special characters
+    content = re.sub(r'\[a-z\]', '', content)
+
     replace_list = [' ', ',', '\n', '\r', '\u3000', '“', '”',
-                    '未完待續', '*', '未完待續', '．', '.', '�', 'ｕａｎｕ', 'ＵU看書']
+                    '未完待續', '*', '未完待續', '.', '.', '�', 'uanu', 'UU看書']
+
     for i in replace_list:
         content = content.replace(i, '\r')
-        data_list = content.split('\r')
-        data_list = [x for x in data_list if x != '']  # 去除空字串
+
+    data_list = content.split('\r')
+    data_list = [x for x in data_list if x != '']
 
     return timu, next_link, data_list
+
+# Text-to-speech and save
 
 
 async def main(output, text):
@@ -62,25 +77,32 @@ async def main(output, text):
     tts = edge_tts.Communicate(text, voice)
     await tts.save(output)
 
+if __name__ == '__main__':
 
-if '__main__' == __name__:
-    text_folder = './text_file'  # txt output folder
+    text_folder = './text_file'  # Text file output folder
+
     url = input('Path your start uu novel url:')
 
-    if not url == '':
+    if url != '':
+
         copy_page = int(input('How many pages do you want to copy:'))
 
-        if type(copy_page) is int:
-            # create folder if not exist
+        if isinstance(copy_page, int):
+
+            # Create folder if not exist
             if not os.path.exists(text_folder):
                 os.makedirs(text_folder)
-            # start copy
+
+            # Start copying
             for i in range(copy_page):
+
                 title, url, text = page_data(url)
                 text = ''.join(text)
+
                 print(f'start copy {title} : {now.strftime("%H:%M:%S")}')
+
                 if not os.path.exists(f'./audio_file/{title}.wav'):
                     asyncio.run(main(f'./audio_file/{title}.wav', text))
+
                 print(
-                    f'copy {title} done,time cost : {str(datetime.now() - now).split(".")[0]}')
-pass
+                    f'copy {title} done, time cost : {str(datetime.now() - now).split(".")[0]}')
